fdef968e368db8984e7e375ae54e23f71f83b73e
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-02-14 11:08:42 +0100

    Update app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,54 +4,48 @@ import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 
-export class AppComponent implements AfterContentInit, OnInit {
-
+export class AppComponent implements OnInit {
   constructor(
     public translate: TranslateService,
-    private cookieService: CookieService,
+    private cookieService: CookieService
   ) {
-    translate.addLangs(['pl', 'en', 'du']);
+    translate.addLangs(['pl', 'en']);
     translate.setDefaultLang(localStorage.getItem('storageLang')!);
   }
 
   checkCookies() {
-    if (this.cookieService.get('acceptedCookies') === 'yes') {
+    if (this.cookieService.get('acceptedCookies') === 'yes') 
       return false;
-    }
+
     return true;
   }
 
   checkLang() {
-    if (!localStorage.getItem('storageLang') || localStorage.getItem('storageLang') == undefined) {
+    if (
+      !localStorage.getItem('storageLang') ||
+      localStorage.getItem('storageLang') == undefined
+    )
       localStorage.setItem('storageLang', 'pl');
-    }
   }
 
   checkFontSize() {
+    let fontSize = localStorage.getItem('fontSize');
     if (
-      localStorage.getItem('fontSize') !== 'li' &&
-      localStorage.getItem('fontSize') !== 'sm' &&
-      localStorage.getItem('fontSize') !== 'md' &&
-      localStorage.getItem('fontSize') !== 'bg' &&
-      localStorage.getItem('fontSize') !== 'hg'
-    ) {
+      fontSize !== 'li' &&
+      fontSize !== 'sm' &&
+      fontSize !== 'md' &&
+      fontSize !== 'bg' &&
+      fontSize !== 'hg'
+    )
       localStorage.setItem('fontSize', 'li');
-    }
-  }
-  
-  ngAfterContentInit(): void {
-    if (localStorage.getItem('fontSize') == undefined) {
-      localStorage.setItem('fontSize', 'li');
-    }
-    this.checkFontSize();
   }
 
   ngOnInit(): void {
     this.checkLang();
     this.checkCookies();
+    this.checkFontSize();
   }
-
 }
