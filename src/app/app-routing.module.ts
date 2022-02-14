2ac527d8739158955cec5caae48acb03a19916df
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-02-14 13:39:32 +0100

    Update regulations component, pl.json, styling, angular.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+// Import pages
 import { PageNotFoundComponent } from './modules/pages/page-not-found/page-not-found.component'
 import { HomeComponent } from './modules/pages/home/home.component'
 import { RegulationsComponent } from './modules/pages/regulations/regulations.component';
 import { CookiesComponent } from './modules/pages/cookies/cookies.component';
 
 const routes: Routes = [
-  { path: 'Regulamin', component: RegulationsComponent },
+  { path: 'Polityka', component: RegulationsComponent },
   { path: 'Cookies', component: CookiesComponent },
   { path: '', component: HomeComponent },
   { path: '**', component: PageNotFoundComponent },
