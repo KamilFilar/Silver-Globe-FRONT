c6787e9c42532dfdae021833c99f8da262b6e01f
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-02-14 14:16:05 +0100

    Update page-not-found component

--- src/app/modules/pages/page-not-found/page-not-found.component.ts
+++ src/app/modules/pages/page-not-found/page-not-found.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
@@ -11,16 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PageNotFoundComponent implements OnInit {
-
-  err = 'ERROR 404'
+  errorInfo = 'ERROR 404'
   img = '../../../../assets/images/page-not-found/UFO2.gif';
-  casueInfo = 'Prawdopodobne przyczyny błędu:';
-  casues = [
-    'Niewłaściwy adres URL strony lub podstrony.',
-    'Strona może być chwilowo niedostępna z uwagi na wprowadzane zmiany.',
-    'Niewyczyszczone pliki cache (pamięci podręcznej) i pliki cookies (ciasteczka) przeglądarki.',
-    'Złośliwe oprogramowanie, które wymusza komunikat błędu na określonych stronach i ich podstronach.'
-  ];
   btnHref = '';
 
   constructor() { }
