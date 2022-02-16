570b9886c472be3f2e9e7c4b0b3952da6401a918
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-02-16 10:25:11 +0100

    Update home-contact component

--- src/app/modules/pages/home/home-contact/home-contact.component.ts
+++ src/app/modules/pages/home/home-contact/home-contact.component.ts
@@ -5,49 +5,49 @@ import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home-contact',
   templateUrl: './home-contact.component.html',
-  styleUrls: ['./home-contact.component.scss', './../../../../../assets/styling/global/fontSize.scss']
+  styleUrls: [
+    './home-contact.component.scss',
+    './../../../../../assets/styling/global/fontSize.scss',
+  ],
 })
 
 export class HomeContactComponent implements OnInit {
-
   faInfoCircle = faInfoCircle;
   form!: FormGroup;
   msgState!: number; // 1 = required, 2 = invalid, 3 = success
   msgIconPath!: string;
   showError = {
     email: false,
-    content: false
-  }
+    content: false,
+  };
   user = {
     email: '',
-    content: ''
-  }
+    content: '',
+  };
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   get f(): { [key: string]: AbstractControl } {
     return this.form.controls;
   }
 
-  sendUserEmail(){
-    let email = (<HTMLInputElement> document.getElementById('userEmail')).value;
-    let subject = (<HTMLInputElement> document.getElementById('userSubject')).value;
-    let content = (<HTMLInputElement> document.getElementById('userContent')).value;
+  sendUserEmail() {
+    let email = (<HTMLInputElement>document.getElementById('userEmail')).value;
+    let subject = (<HTMLInputElement>document.getElementById('userSubject')).value;
+    let content = (<HTMLInputElement>document.getElementById('userContent')).value;
 
     this.user.email = email;
-    this.user.content = content
+    this.user.content = content;
 
-    if(this.showError.email === true && email){
+    if (this.showError.email === true && email) {
       this.showError.email = false;
     }
 
-    if(this.showError.content === true && content){
+    if (this.showError.content === true && content) {
       this.showError.content = false;
     }
 
-    if(!email && !content){
+    if (!email && !content) {
       this.msgState = 1;
       this.msgIconPath = './../../../../../assets/images/materials/emoji/face-concerned.png';
       this.showError.email = true;
@@ -55,14 +55,14 @@ export class HomeContactComponent implements OnInit {
       return;
     }
 
-    if(!email){
+    if (!email) {
       this.msgState = 1;
       this.msgIconPath = './../../../../../assets/images/materials/emoji/face-concerned.png';
       this.showError.email = true;
       return;
     }
 
-    if(!content){
+    if (!content) {
       this.msgState = 1;
       this.msgIconPath = './../../../../../assets/images/materials/emoji/face-concerned.png';
       this.showError.content = true;
@@ -76,9 +76,9 @@ export class HomeContactComponent implements OnInit {
       return;
     }
 
-    if(email && content){
-      if(!subject){
-        subject = 'MoonPhase user: '+email;
+    if (email && content) {
+      if (!subject) {
+        subject = 'MoonPhase user: ' + email;
       }
       this.msgState = 3;
       this.msgIconPath = './../../../../../assets/images/materials/emoji/happy.png';
@@ -86,27 +86,23 @@ export class HomeContactComponent implements OnInit {
     }
   }
 
-
-
-  fontSizeFunc(){
+  fontSizeFunc() {
     let fontSize = localStorage.getItem('fontSize')!;
     return fontSize;
   }
 
   ngOnInit(): void {
     this.fontSizeFunc();
-    this.form = this.formBuilder.group(
-      {
-        email: ['', [
+    this.form = this.formBuilder.group({
+      email: [
+        '',
+        [
           Validators.required,
           Validators.email,
-          Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$')
-        ]],
-        textarea: ['', [
-          Validators.required
-        ]]
-      },
-    );
+          Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
+      ],
+      textarea: ['', [Validators.required]],
+    });
   }
-
 }
